427da1dbb8490d25a86dff95d4b536d23a62b29e
larrylud <larrylud@5f1fc678-44d4-42fd-abb1-d1f35cc5ac26>
2012-04-25 16:50:12 +0000

    fixing manifest to be manifests

--- modules/mailforward/manifests/init.pp
+++ modules/mailforward/manifests/init.pp
@@ -3,9 +3,9 @@
 class mailforward {
 
 	file { "/root/.forward":
-			owner => 'root',
-			group => 'root',
-			mode => '0644',
+        	owner => 'root',
+	        group => 'root',
+	        mode  => '0440',
 		source => "puppet:///files/mailforward/forward",
 	}
 }
