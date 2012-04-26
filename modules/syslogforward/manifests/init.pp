be83ee4da3cd86b5b6eb634b573e824c1d80ab9e
larrylud <larrylud@5f1fc678-44d4-42fd-abb1-d1f35cc5ac26>
2012-04-26 14:00:46 +0000

    added syslogforward/manifests/init.pp and modified to reflect proper file names

--- modules/syslogforward/manifests/init.pp
+++ modules/syslogforward/manifests/init.pp
@@ -2,29 +2,26 @@
 
 class syslogforward {
 	case $operatingsystem {
-		SLES: {file { "/etc/syslog-ng/syslog.conf":
-					ensure => file,
+		SLES: {file { "/etc/syslog-ng/syslog-ng.conf":
 					owner => "root",
 					group => "root",
 					mode => 0644,
-					source => "puppet:///files/syslogforward/SLES/syslog.conf"
+					source => "puppet:///files/syslogforward/SLES/syslog.conf",
 			}
 		}
 		RedHat: {case $lsbmajdistrelease {
 			5: {file { "/etc/syslog.conf":
-					ensure => file,
 					owner => "root",
 					group => "root",
 					mode => 0644,
-					source => "puppet:///files/syslogforward/RedHat/syslog.conf"
+					source => "puppet:///files/syslogforward/RedHat/syslog.conf",
 					}
 				}
 			6: {file { "/etc/rsyslog.conf":
-					ensure => file,
 					owner => "root",
 					group => "root",
 					mode => 0644,
-					source => "puppet:///files/syslogforward/RedHat/rsyslog.conf"
+					source => "puppet:///files/syslogforward/RedHat/rsyslog.conf",
 					}
 				}
 			default: { fail("Non provisioned version of Red Hat") }
