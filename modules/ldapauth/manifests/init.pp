6cba40cbc927f0f567fd7a9b7ca17a1d0986068d
larrylud <larrylud@5f1fc678-44d4-42fd-abb1-d1f35cc5ac26>
2012-04-27 17:44:31 +0000

    updated ldap_authentication to reflect seperate OS and versions, removed puppet copying files and setting up ldap and just launching the ldap_authentication script

--- modules/ldapauth/manifests/init.pp
+++ modules/ldapauth/manifests/init.pp
@@ -1,70 +1,29 @@
-# /linuxdist/puppet/modules/ldapauth/manifests/init.pp
+# /etc/puppet/modules/ldapauth/manifests/init.pp
 
 class ldapauth {
 
-	file { "/etc/pam.d/login":
+	file { "/tmp/ldap_authentication":
 		ensure => file,
         	owner => 'root',
 	        group => 'root',
-	        mode  => '0440',
-		source => "puppet:///files/ldapauth/$operatingsystem/login"
+	        mode  => '0740',
+		source => "puppet:///files/ldapauth/$operatingsystem/$lsbmajdistrelease/ldap_authentication"
     }
 
-        file { "/etc/pam.d/su":
-                ensure => file,
-                owner => 'root',
-                group => 'root',
-                mode  => '0440',
-                source => "puppet:///files/ldapauth/$operatingsystem/su"
-	}
-
-        file { "/etc/pam.d/sshd":
-                ensure => file,
-                owner => 'root',
-                group => 'root',
-                mode  => '0440',
-                source => "puppet:///files/ldapauth/$operatingsystem/sshd"
-        }
-
-	file { "/etc/ldap.conf":
-                ensure => file,
-                owner => 'root',
-                group => 'root',
-                mode  => '0440',
-                source => "puppet:///files/ldapauth/$operatingsystem/ldap.conf"
-        }
-
-	file { "/etc/crypt":
-		ensure => directory,
-		owner => 'root',
-		group => 'root',
-		mode => '0400'
+# execute ldap script 
+	exec { "/tmp/ldap_authentication":
+		cwd => "/tmp",
+		path => "/usr/bin:/usr/sbin:/bin",
+		subscribe => File["/tmp/ldap_authentication"],
+		refreshonly => true
 	}
 	
-	exec { "copy-passwd":
-		command => "cp /etc/passwd /etc/crypt/passwd_local",
-		cwd => "/etc",
-	}
-
-        exec { "copy-shadow":
-                command => "cp /etc/shadow /etc/crypt/shadow_local",
-                cwd => "/etc",
-        }
-
-        file { "/etc/crypt/validate_user_passwd.sh":
-                ensure => file,
-                owner => 'root',
-                group => 'root',
-                mode  => '0700',
-                source => "puppet:///files/ldapauth/$operatingsystem/validate_user_passwd.sh"
-        }
-
 	cron { ldap_auth:
 		command => "/etc/crypt/validate_user_passwd.sh",
 		user => root,
 		hour => 2
 	}
-
+	
 # execute validate users script only when it changes
 	exec { "/etc/crypt/validate_user_passwd.sh":
 		cwd => "/etc/crypt",
@@ -72,6 +31,5 @@ class ldapauth {
 		subscribe => File["/etc/crypt/validate_user_passwd.sh"],
 		refreshonly => true
 	}
-
 }
 
