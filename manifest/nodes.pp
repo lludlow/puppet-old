ff67ba907ddc5cf81fa00d1573ce6f61b4e54933
larrylud <larrylud@5f1fc678-44d4-42fd-abb1-d1f35cc5ac26>
2012-04-30 19:40:37 +0000

    added modules and files for locate, logrotate, and slp configurations. Added node config for lnxmgt and slp servers

--- manifest/nodes.pp
+++ manifest/nodes.pp
@@ -2,8 +2,17 @@
 
 
 node basenode {
-        include sudo, ldapauth, mailforward, setntp, setbanner, rootprofile, syslogforward, sshkeys
+        include sudo, ldapauth, mailforward, setntp, setbanner, rootprofile, syslogforward, sshkeys, locate
 }
 
 node default inherits basenode {
 }
+
+node /^lnxmgt/ {
+include setbanner, rootprofile, syslogforward, locate, sudo, mailforward
+}
+
+node /^slp\d+$/ {
+include sudo, ldapauth, mailforward, setntp, setbanner, rootprofile, syslogforward, sshkeys, locate
+}
+
