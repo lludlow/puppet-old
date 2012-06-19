68735619f0accbc42f1b80f85d3964efbca0b0ee
larrylud <larrylud@5f1fc678-44d4-42fd-abb1-d1f35cc5ac26>
2012-06-19 15:40:28 +0000

    promoting preprod syslogforward manifest as approved by stevem, sivakuma, and tstrauch

--- modules/syslogforward/manifests/init.pp
+++ modules/syslogforward/manifests/init.pp
@@ -1,27 +1,59 @@
-# /etc/puppet/modules/syslogforward/manifest/init.pp
-
+# /etc/puppet/modules/syslogforward/manifests/init.pp
+##
+##LNXMGT servers are excluded in nodes.pp from the syslogforward manifest since they are the syslog servers.
+##
 class syslogforward {
+
 	case $operatingsystem {
 		SLES: {file { "/etc/syslog-ng/syslog-ng.conf":
 					owner => "root",
 					group => "root",
-					mode => 0644,
+					mode => '0644',
 					source => "puppet:///files/syslogforward/SLES/syslog.conf",
 			}
+			exec { "/etc/init.d/syslog restart":
+			cwd => "/etc/init.d",
+			path => "/usr/bin:/usr/sbin:/bin",
+			subscribe => File["/etc/syslog-ng/syslog-ng.conf"],
+			refreshonly => true
+			}
 		}
+		
 		RedHat: {case $lsbmajdistrelease {
-			5: {file { "/etc/syslog.conf":
+			5: {file { "/etc/rsyslog.conf":
 					owner => "root",
 					group => "root",
-					mode => 0644,
-					source => "puppet:///files/syslogforward/RedHat/syslog.conf",
+					mode => '0644',
+					source => "puppet:///files/syslogforward/RedHat/rsyslog.conf",
 					}
+					package { "rsyslog":
+						ensure => "installed",
+					}
+					exec { "/etc/init.d/rsyslog restart":
+					cwd => "/etc/init.d",
+					path => "/usr/bin:/usr/sbin:/bin",
+					subscribe => File["/etc/rsyslog.conf"],
+					require => Exec ["/etc/init.d/syslog stop"],
+					refreshonly => true,
+					}
+					exec { "/etc/init.d/syslog stop":
+					cwd => "/etc/init.d",
+					require => Package ["rsyslog"],
+					}
+
 				}
+				
 			6: {file { "/etc/rsyslog.conf":
 					owner => "root",
 					group => "root",
-					mode => 0644,
+					mode => '0644',
 					source => "puppet:///files/syslogforward/RedHat/rsyslog.conf",
+					}
+					exec { "/etc/init.d/rsyslog restart":
+					cwd => "/etc/init.d",
+					path => "/usr/bin:/usr/sbin:/bin",
+					subscribe => File["/etc/rsyslog.conf"],
+					refreshonly => true
 					}
 				}
 			default: { fail("Non provisioned version of Red Hat") }
