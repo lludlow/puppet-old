27ccd0415fb9773bee0a7f26bfcfb8191d5647b6
larrylud <larrylud@5f1fc678-44d4-42fd-abb1-d1f35cc5ac26>
2012-06-25 15:17:10 +0000

    added puppet-run policy, approved by tstrauch and sivakuma

--- manifest/nodes.pp
+++ manifest/nodes.pp
@@ -2,24 +2,24 @@
 
 
 node basenode {
-        include sudo, ldapauth, mailforward, setntp, setbanner, rootprofile, syslogforward, sshkeys, locate, stophpsmh
+        include sudo, ldapauth, mailforward, setntp, setbanner, rootprofile, syslogforward, sshkeys, locate, stophpsmh, puppet-run
 }
 
 node default inherits basenode {
 }
 
 node /^lnxmgt/ {
-include setbanner, rootprofile, locate, sudo, mailforward
+include setbanner, rootprofile, locate, sudo, mailforward, puppet-run
 }
 
 node /^slp\d+$/ {
-include sudo, ldapauth, mailforward, setntp, setbanner, rootprofile, syslogforward, sshkeys, locate
+include sudo, ldapauth, mailforward, setntp, setbanner, rootprofile, syslogforward, sshkeys, locate, puppet-run
 }
 
 node /^lnxvom-prod01/ {
-        include sudo, mailforward, setntp, setbanner, rootprofile, syslogforward, locate, stophpsmh
+        include sudo, mailforward, setntp, setbanner, rootprofile, syslogforward, locate, stophpsmh, puppet-run
 }
 
 node /^netapp-prod01/ {
-        include sudo, ldapauth, mailforward, setntp, setbanner, rootprofile, sshkeys, locate, stophpsmh
+        include sudo, ldapauth, mailforward, setntp, setbanner, rootprofile, sshkeys, locate, stophpsmh, puppet-run
 }
---
+++ modules/puppet-run/manifests/init.pp
@@ -0,0 +1,26 @@
+# /etc/puppet/modules/puppet-run/manifests/init.pp
+
+class puppet-run {
+ $first = fqdn_rand(20)
+ $second = fqdn_rand(20) + 30
+
+	file { "/etc/cleancron":
+		owner => "root",
+		group => "root",
+		mode => 0600,
+		source => "puppet:///files/etc/cleancron",
+	}
+	
+	exec {clean-cron:
+		command => "crontab -l | sed '\/15/d' | crontab -",
+		subscribe => File["/etc/cleancron"],
+		refreshonly => true,
+		}
+		
+	cron { puppet-run:
+		command => "/usr/bin/puppet agent --onetime > /dev/null",
+		user => "root",
+		minute => [ $first, $second ],
+		ensure => present,
+	}
+}
