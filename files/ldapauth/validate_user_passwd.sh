3e6a22427b3023ea2f4b8095678360da19800aed
sivakuma <sivakuma@5f1fc678-44d4-42fd-abb1-d1f35cc5ac26>
2012-08-14 19:01:47 +0000

    changed log file to have time stamp.

--- files/ldapauth/validate_user_passwd.sh
+++ files/ldapauth/validate_user_passwd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+ 
 #------------------------------------------
 # kumar 08/17/2007
 #
@@ -16,7 +16,8 @@ LOCALPASSWD_FILE="/etc/crypt/passwd_local"
 LOCALSHADOW_FILE="/etc/crypt/shadow_local"
 EMAILTOADDRESS="ots-linux-osadmin@smail-int.med.umich.edu"
 EMAILSUBJECT="Warning !!  password merge did not complete for $HOSTNAME"
-LOGFILE="/var/log/passwd_merge`date +%y%m%d%H%M%S`.log"
+TIMESTAMP=`date +"%b %d %Y %H:%M:%S"`
+LOGFILE="/var/log/passwd_merge.log"
 
 if [ ! -d "${MOUNTPOINT}" ]; then
    mkdir $MOUNTPOINT
@@ -36,13 +37,13 @@ sleep 3
 
 MTAB=`grep $MOUNTPOINT /etc/mtab`
 if [ -z "${MTAB}" ]; then
-   echo " Problem mounting linuxdist, password merging stopped" >> $LOGFILE
+   echo "$TIMESTAMP Problem mounting linuxdist, password merging stopped" >> $LOGFILE
    echo "Please check and fix the linuxdist mount issue on $HOSTNAME before proceeding further" | mail "-s $EMAILSUBJECT" $EMAILTOADDRESS  
    exit
 fi
 
 if [ ! -e "$MOUNTPOINT/user_maint/$HOSTNAME" ]; then
-   echo "user password file $HOSTNAME missing on $MOUNTPOINT/user_maint, please create it before running the script" >> $LOGFILE
+   echo "$TIMESTAMP user password file $HOSTNAME missing on $MOUNTPOINT/user_maint, please create it before running the script" >> $LOGFILE
    echo "user password file $HOSTNAME missing on $MOUNTPOINT/user_maint" | mail "-s $EMAILSUBJECT" $EMAILTOADDRESS  
    exit
 fi
@@ -50,7 +51,7 @@ fi
 IDS=`cat $MOUNTPOINT/user_maint/$HOSTNAME | tr :  " " | awk  '{print $3}'`
 for ID in $IDS; do
         if [ $ID -le "$LIMIT" ];  then
-           echo " Users have UID less than 1001, password merging stopped" >> $LOGFILE
+           echo "$TIMESTAMP Users have UID less than 1001, password merging stopped" >> $LOGFILE
            echo "Users have UID less than 1001.warning password merge did not complete for $HOSTNAME" | mail "-s $EMAILSUBJECT" $EMAILTOADDRESS  
 #           echo "id is " $ID
            exit 1
@@ -61,7 +62,7 @@ NAMES=`cat $MOUNTPOINT/user_maint/$HOSTNAME | tr : " " | awk '{print $1}'`
 for NAME in $NAMES; do
        SHAD=`grep $NAME /etc/shadow`
        if [ -n "${SHAD}" ]; then
-           echo " Shadow entry exists for the user, user $NAME deleted from shadow file" >> $LOGFILE
+           echo "$TIMESTAMP Shadow entry exists for the user, user $NAME deleted from shadow file" >> $LOGFILE
            echo "User $NAME exist in the Shadow file on $HOSTNAME, user deleted from shadow file" | mail "-s $EMAILSUBJECT" $EMAILTOADDRESS  
 #           echo "Name is " $NAME
            sed "/$NAME/d" /etc/shadow >> /etc/shadow.script
@@ -73,7 +74,7 @@ NAMES=`cat $MOUNTPOINT/user_maint/$HOSTNAME | tr : " " | awk '{print $1}'`
 for NAME in $NAMES; do
        MSYS=`grep $NAME $MOUNTPOINT/user_maint/master_sys`
        if [ -n "${MSYS}" ]; then
-           echo " User exist in the master system account file, password merging stopped" >> $LOGFILE
+           echo "$TIMESTAMP User exist in the master system account file, password merging stopped" >> $LOGFILE
            echo "User $NAME exist in the master_sys file, so it could be a system account" | mail "-s $EMAILSUBJECT" $EMAILTOADDRESS  
 #           echo "Name is " $NAME
            exit 1
@@ -92,9 +93,9 @@ for NAME in $NAMES; do
        if [ "${MUSR}" ]; then
            cat $LOCALPASSWD_FILE $MOUNTPOINT/user_maint/$HOSTNAME > /etc/passwd 
            cp $LOCALSHADOW_FILE /etc/shadow
-           echo " User $NAME exists in the master user account file" >> $LOGFILE
+           echo "$TIMESTAMP User $NAME exists in the master user account file" >> $LOGFILE
       else
-           echo " User $NAME does not exist in the master user account file" >> $LOGFILE
+           echo "$TIMESTAMP User $NAME does not exist in the master user account file" >> $LOGFILE
            echo "User $NAME does not exist in the master_user file" | mail "-s $EMAILSUBJECT" $EMAILTOADDRESS  
 #           echo "Name is " $NAME
            exit 1
